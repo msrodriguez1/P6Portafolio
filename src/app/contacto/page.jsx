daac14fbeb93593deea34f6bda0b8dcccc288a8e
María Rodriguez <marodriguez.dwfsc6@bootcampudd.cl>
2023-10-01 21:53:01 -0500

    cambios varios

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -10,9 +10,13 @@ export default function page() {
   return (
     <div>
       <Header />
-      <main>
-        <h2>Contacto</h2>
-        <p>Información de contacto...</p>
+      <main className="h-screen flex flex-column justify-center items-center">
+        <div className=" flex-column justify-between w-2/3 h-2/3 rounded-md shadow-lg bg-white p-4">
+        <h2>mail: maria.rodriguezalliende@gmail.com</h2>
+        <h2>celular: +56995533525</h2>
+        <h2>linkedIn: https//:linkedinurlprueba.cl</h2>
+
+        </div>
       </main>
       <Footer />
     </div>
--- src/app/proyectos/page.jsx
+++ src/app/proyectos/page.jsx
@@ -14,40 +14,49 @@ export default function page() {
 
       <main className="flex-grow w-full flex flex-col justify-between">
         <div className="text-center mb-4 mt-16">
-          <h1>Mis Proyectos</h1>
+          <h1 className="text-xl font-semibold">Mis Proyectos</h1>
         </div>
 
-        <div className="flex-grow flex flex-col justify-between">
-          <div className="flex flex-col  mb-16">
-            <h1 className="mb-8">Métricas en Tiempo Real</h1>
+        <div className="flex-grow flex flex-col justify-between p-4 ">
+          <div className="flex flex-row space-x-4 mt-6 rounded-md shadow-lg bg-white p-4">
+            <div  className="flex flex-col">
+            <h1 className=" font-medium mb-8 text-base">Métricas en Tiempo Real</h1>
             <img src="/Migracion.png" alt="Migración" className="w-1/3 h-auto" />
-            <ul  className="list-disc list-inside pl-5">
+            </div>
+            <ul  className="list-disc list-inside pl-5 mt-4 text-sm">
               <li>Llamada a API de migración entre países</li>
               <li>Filtros para facilitar la búsqueda de datos</li>
               <li>Uso de recursos visuales para mostrar la data como gráficos</li>
-
+              <li> Principales Tecnologías usadas: Javascript, Librería Chart.js, Librería Axxios</li>
             </ul>
           </div>
 
-          <div className="flex flex-col  mb-16">
-            <h1 className="mb-8">Sitio Web Restaurant</h1>
+          <div className="flex flex-row mt-6  space-x-4  rounded-md overflow-hidden shadow-lg bg-white p-4">
+          <div  className="flex flex-col">
+
+            <h1 className=" font-medium mb-8 text-base">Sitio Web Restaurant</h1>
             <img src="/Anakena.png" alt="Anakena" className="w-1/3 h-auto" />
-            <ul  className="list-disc list-inside pl-5">
-              <li>Sitio web multipágina</li>
+            </div>
+            <ul  className="list-disc list-inside pl-5 mt-4 text-sm">
+              <li>Sitio web multipágina: página de inicio, inspiraciones, carta y nosotros.</li>
               <li>Componente de gestión de reservas para el usuario(CRUD)</li>
-              <li>Uso de bases de datos Firebase</li>
+              <li> Principales Tecnologías usadas: Javascript, Firebase</li>
 
             </ul>
           </div>
 
-          <div className="flex flex-col  mb-16">
-            <h1 className="mb-8">Perfumería Online</h1>
+          <div className="flex flex-row space-x-4 mt-6 rounded-md overflow-hidden shadow-lg bg-white p-4">
+          <div  className="flex flex-col">
+
+            <h1 className=" font-medium mb-8 text-base">Perfumería Online</h1>
             <img src="/Lelabo.png" alt="Le Labo" className="w-1/3 h-auto" />
-            <ul  className="list-disc list-inside pl-5">
+           </div>
+            <ul  className="list-disc list-inside pl-5 mt-4 text-sm">
               <li>Ecommerce de perfumería multipágina: catálogo de productos, página de producto, mi perfil y página de inicio.</li>
               <li>Registro de usuario e inicio de sesión</li>
               <li>Creación y edición de carrito de compras</li>
-              <li>Página de checkout</li>
+              <li>Principales Tecnologías usadas: React, MongoDB</li>
+
             </ul>
           </div>
         </div>
