aa058a038d31c8fe6d61037e500497e5657d1816
María Rodriguez <marodriguez.dwfsc6@bootcampudd.cl>
2023-10-07 07:18:40 -0500

    algunos cambios

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -11,11 +11,13 @@ export default function page() {
     <div>
       <Header />
       <main className="h-screen flex flex-column justify-center items-center">
-        <div className=" flex-column justify-between w-2/3 h-2/3 rounded-md shadow-lg bg-white p-4">
-        <h2>mail: maria.rodriguezalliende@gmail.com</h2>
-        <h2>celular: +56995533525</h2>
-        <h2>linkedIn: https//:linkedinurlprueba.cl</h2>
-
+        <div className=" flex-column justify-center items-center w-1/3 h-1/4 rounded-md shadow-lg bg-white p-4">
+        <h2>Contactame</h2>
+          <div className="flex-column justify-center items-center">
+        <p>mail: maria.rodriguezalliende@gmail.com</p>
+        <p>celular: +56995533525</p>
+        <p>linkedIn: https//:linkedinurlprueba.cl</p>
+        </div>
         </div>
       </main>
       <Footer />
